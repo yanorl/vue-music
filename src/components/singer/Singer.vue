ecce77acc180d5cf0740df857b245047f9386850
yan <625620461@qq.com>
2019-05-16 18:57:10 +0800

    配置项目准备工作

---
+++ src/components/singer/Singer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="singer-box">
+   歌手
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'singer-box',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
