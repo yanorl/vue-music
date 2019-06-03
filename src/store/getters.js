88205eab630727676b04197502c53ac5f58fc7b6
yan <625620461@qq.com>
2019-06-03 18:53:50 +0800

    播放器播放进度条

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,6 +5,6 @@ export const playlist = state => state.playlist
 export const sequenceList = state => state.sequenceList
 export const mode = state => state.mode
 export const currentIndex = state => state.currentIndex
-export const curentSong = (state) => {
+export const currentSong = (state) => {
   return state.playlist[state.currentIndex] || {}
 }
